41e8bde4b8e1dfb1edd1b88020840503c4f22f5d
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2019-05-13 18:41:20 -0700

    impl3: update q2 with enhancements from q1

--- impl3/q2.py
+++ impl3/q2.py
@@ -1,5 +1,5 @@
-# q2: same as q1 but using relu instead of sigmoid
-
+import os
+import sys
 
 import torch
 import torch.nn as nn
@@ -15,15 +15,30 @@ if torch.cuda.is_available():
     device = torch.device('cuda')
 else:
     device = torch.device('cpu')
-    
+
 print('Using PyTorch version:', torch.__version__, ' Device:', device)
 
 batch_size = 32
+learning_rate = 0.01
+epochs = 20
 
-train_dataset = datasets.CIFAR10('./data', train=True, download=True, transform=transforms.ToTensor())
-validation_dataset = datasets.CIFAR10('./data', train=False, transform=transforms.ToTensor()) 
-train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True) 
+if len(sys.argv) > 1:
+    learning_rate = float(sys.argv[1])
+
+# DONE: scale input features? idk, seems to already be done
+# DONE: dump model parameters after each epoch
+# TODO: plots
+
+# output: (data for) plots of training loss and validation error as a function of training epoch,
+# for a variety of learning rates
+
+core_dataset = datasets.CIFAR10('./data', train=True, download=True, transform=transforms.ToTensor())
+train_dataset = torch.utils.data.Subset(core_dataset, range(0, 40000))
+validation_dataset = torch.utils.data.Subset(core_dataset, range(40000, 50000))
+#test_dataset = datasets.CIFAR10('./data', train=False, transform=transforms.ToTensor())
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
 validation_loader = torch.utils.data.DataLoader(dataset=validation_dataset, batch_size=batch_size, shuffle=False)
+#test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
 
 for (X_train, y_train) in train_loader:
     print('X_train:', X_train.size(), 'type:', X_train.type())
@@ -51,8 +66,9 @@ class Net(nn.Module):
         x = F.log_softmax(self.fc3(x), dim=1)
         return x
 
+# TODO: play around with learning rate
 model = Net().to(device)
-optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 criterion = nn.CrossEntropyLoss()
 
 print(model)
@@ -60,7 +76,7 @@ print(model)
 def train(epoch, log_interval=200):
     # Set model to training mode
     model.train()
-    
+
     # Loop over each batch from the training set
     for batch_idx, (data, target) in enumerate(train_loader):
         # Copy data to GPU if needed
@@ -68,8 +84,8 @@ def train(epoch, log_interval=200):
         target = target.to(device)
 
         # Zero gradient buffers
-        optimizer.zero_grad() 
-        
+        optimizer.zero_grad()
+
         # Pass data through the network
         output = model(data)
 
@@ -78,14 +94,16 @@ def train(epoch, log_interval=200):
 
         # Backpropagate
         loss.backward()
-        
+
         # Update weights
         optimizer.step()
-        
+
         if batch_idx % log_interval == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss.data.item()))
+
+
 
 def validate(loss_vector, accuracy_vector):
     model.eval()
@@ -102,17 +120,31 @@ def validate(loss_vector, accuracy_vector):
     loss_vector.append(val_loss)
 
     accuracy = 100. * correct.to(torch.float32) / len(validation_loader.dataset)
-    accuracy_vector.append(accuracy)
-    
-    print('\nValidation set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+    accuracy_vector.append(float(accuracy))
+
+    print('Validation set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)'.format(
         val_loss, correct, len(validation_loader.dataset), accuracy))
 
-epochs = 5
 
-lossv, accv = [], []
+model_dir = "q2-model"
+try:
+    os.mkdir(model_dir)
+except FileExistsError:
+    pass
+
+lossv, accv = [[],[]], [[],[]]
 for epoch in range(1, epochs + 1):
     train(epoch)
-    validate(lossv, accv)
+    print()
+    validate(lossv[0], accv[0])
+    #test(lossv[1],accv[1])
+    print()
+
+    filename = os.path.join(model_dir, "q2-lr{0:f}-epoch{1:d}".format(learning_rate, epoch))
+    torch.save(model.state_dict(), filename)
+
+print(lossv)
+print(accv)
 
 #plt.figure(figsize=(5,3))
 #plt.plot(np.arange(1,epochs+1), lossv)
