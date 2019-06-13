c7fba7d48578a7a6e7dd7f83434b8d87bf3f1bef
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2019-06-12 21:37:49 -0700

    contest: make filenames configurable

--- contest/randomforest2.py
+++ contest/randomforest2.py
@@ -1,18 +1,29 @@
 #!/usr/bin/env python3
 import sys
-import csv
-import math
-import textwrap
-from collections import Counter
+import argparse
 import numpy
 import sklearn.ensemble
 
 def debug(*args, **kwargs):
     print(*args, **kwargs, file=sys.stderr)
 
+
+def getncols(filename):
+    with open(filename) as f:
+        _ = f.readline()
+        l = f.readline()
+        return l.rstrip().count('\t') + 1
+
 def main():
-    filename = "small.txt"
-    cols = list(range(1,105))
+    parser = argparse.ArgumentParser()
+    parser.add_argument("train_filename", help="path to training data")
+    parser.add_argument("test_filename", help="path to testing data", nargs="?")
+    parser.add_argument("--noclass", help="whether the test file has a class column or not")
+    args = parser.parse_args()
+
+    filename = args.train_filename
+    ncols = getncols(filename)
+    cols = list(range(1,ncols))
     data = numpy.loadtxt(filename, skiprows=1, usecols=cols)
     target = data[:,0].astype(int)
     data = data[:,1:]
@@ -20,24 +31,26 @@ def main():
     debug(data[:1])
     debug(target[:10])
 
-    rf = sklearn.ensemble.RandomForestClassifier(n_estimators=10)
+    rf = sklearn.ensemble.RandomForestClassifier(n_estimators=100)
     debug(rf)
     rf = rf.fit(data, target)
 
-    debug(rf.score(data, target))
+    debug("train:", rf.score(data, target))
 
-    filename = "feature103_Train.txt"
+    if args.test_filename:
+        filename = args.test_filename
     names = numpy.loadtxt(filename, skiprows=1, usecols=[0], dtype=str)
-    testdata = numpy.loadtxt(filename, skiprows=1, usecols=cols)
-    testtarget = testdata[:,0].astype(int)
-    testdata = testdata[:,1:]
+    if args.noclass:
+        cols = range(1,ncols-1)
+        testdata = numpy.loadtxt(filename, skiprows=1, usecols=cols)
+    else:
+        testdata = numpy.loadtxt(filename, skiprows=1, usecols=cols)
+        testtarget = testdata[:,0].astype(int)
+        testdata = testdata[:,1:]
 
-    pred = rf.predict(testdata)
-    acc = sum(a == b for a, b in zip(pred, testtarget))
-    debug(acc/float(len(testdata)))
-    debug(rf.score(testdata, testtarget))
-    debug("neg acc:", rf.score(testdata[testtarget==0], testtarget[testtarget==0]))
-    debug("pos acc:", rf.score(testdata[testtarget==1], testtarget[testtarget==1]))
+        debug("test:", rf.score(testdata, testtarget))
+        debug("neg acc:", rf.score(testdata[testtarget==0], testtarget[testtarget==0]))
+        debug("pos acc:", rf.score(testdata[testtarget==1], testtarget[testtarget==1]))
 
     for name, prob in zip(names, rf.predict_proba(testdata)):
         print(name, prob[1], sep=",")
--- contest/randomforest2_weighted.py
+++ contest/randomforest2_weighted.py
@@ -1,17 +1,27 @@
-
-import csv
-import math
-import textwrap
-from collections import Counter
+#!/usr/bin/env python3
+import sys
+import argparse
 import numpy
 import sklearn.ensemble
 
 def debug(*args, **kwargs):
     print(*args, **kwargs, file=sys.stderr)
 
+def getncols(filename):
+    with open(filename) as f:
+        _ = f.readline()
+        l = f.readline()
+        return l.rstrip().count('\t') + 1
+
 def main():
-    filename = "feature103_Train.txt"
-    cols = list(range(1,105))
+    parser = argparse.ArgumentParser()
+    parser.add_argument("train_filename", help="path to training data")
+    parser.add_argument("test_filename", help="path to testing data", nargs="?")
+    args = parser.parse_args()
+
+    filename = args.train_filename
+    ncols = getncols(filename)
+    cols = list(range(1,ncols))
     data = numpy.loadtxt(filename, skiprows=1, usecols=cols)
     target = data[:,0].astype(int)
     data = data[:,1:]
@@ -28,21 +38,26 @@ def main():
     # the weights should still sum up to len(data)
     debug(sum(weight), len(data))
 
-    rf = sklearn.ensemble.RandomForestClassifier(n_estimators=10)
+    rf = sklearn.ensemble.RandomForestClassifier(n_estimators=100)
     debug(rf)
     rf = rf.fit(data, target, sample_weight=weight)
 
-    debug(rf.score(data, target))
+    debug("train:", rf.score(data, target))
 
-    filename = "small.txt"
+    if args.test_filename:
+        filename = args.test_filename
     names = numpy.loadtxt(filename, skiprows=1, usecols=[0], dtype=str)
-    testdata = numpy.loadtxt(filename, skiprows=1, usecols=cols)
-    testtarget = testdata[:,0].astype(int)
-    testdata = testdata[:,1:]
+    if args.noclass:
+        cols = range(1,ncols-1)
+        testdata = numpy.loadtxt(filename, skiprows=1, usecols=cols)
+    else:
+        testdata = numpy.loadtxt(filename, skiprows=1, usecols=cols)
+        testtarget = testdata[:,0].astype(int)
+        testdata = testdata[:,1:]
 
-    debug(rf.score(testdata, testtarget))
-    debug("neg acc:", rf.score(testdata[testtarget==0], testtarget[testtarget==0]))
-    debug("pos acc:", rf.score(testdata[testtarget==1], testtarget[testtarget==1]))
+        debug(rf.score(testdata, testtarget))
+        debug("neg acc:", rf.score(testdata[testtarget==0], testtarget[testtarget==0]))
+        debug("pos acc:", rf.score(testdata[testtarget==1], testtarget[testtarget==1]))
 
     for name, prob in zip(names, rf.predict_proba(testdata)):
         print(name, prob[1], sep=",")
