2d254cd41cc22254f1bc408a186691db12816800
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2019-06-12 20:23:28 -0700

    contest: don't print junk to stdout

--- contest/randomforest2.py
+++ contest/randomforest2.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
+import sys
 import csv
 import math
 import textwrap
 from collections import Counter
 import numpy
 import sklearn.ensemble
+
+def debug(*args, **kwargs):
+    print(*args, **kwargs, file=sys.stderr)
 
 def main():
     filename = "small.txt"
@@ -13,16 +17,14 @@ def main():
     target = data[:,0].astype(int)
     data = data[:,1:]
     #print(names[:10])
-    print(data[:1])
-    print(target[:10])
+    debug(data[:1])
+    debug(target[:10])
 
     rf = sklearn.ensemble.RandomForestClassifier(n_estimators=10)
-    print(rf)
+    debug(rf)
     rf = rf.fit(data, target)
 
-    pred = rf.predict(data)
-    acc = sum(a == b for a, b in zip(pred, target))
-    print(acc/float(len(data)))
+    debug(rf.score(data, target))
 
     filename = "feature103_Train.txt"
     names = numpy.loadtxt(filename, skiprows=1, usecols=[0], dtype=str)
@@ -32,10 +34,10 @@ def main():
 
     pred = rf.predict(testdata)
     acc = sum(a == b for a, b in zip(pred, testtarget))
-    print(acc/float(len(testdata)))
-    print(rf.score(testdata, testtarget))
-    print("neg acc:", rf.score(testdata[testtarget==0], testtarget[testtarget==0]))
-    print("pos acc:", rf.score(testdata[testtarget==1], testtarget[testtarget==1]))
+    debug(acc/float(len(testdata)))
+    debug(rf.score(testdata, testtarget))
+    debug("neg acc:", rf.score(testdata[testtarget==0], testtarget[testtarget==0]))
+    debug("pos acc:", rf.score(testdata[testtarget==1], testtarget[testtarget==1]))
 
     for name, prob in zip(names, rf.predict_proba(testdata)):
         print(name, prob[1], sep=",")
--- contest/randomforest2_weighted.py
+++ contest/randomforest2_weighted.py
@@ -6,30 +6,33 @@ from collections import Counter
 import numpy
 import sklearn.ensemble
 
+def debug(*args, **kwargs):
+    print(*args, **kwargs, file=sys.stderr)
+
 def main():
     filename = "feature103_Train.txt"
     cols = list(range(1,105))
     data = numpy.loadtxt(filename, skiprows=1, usecols=cols)
     target = data[:,0].astype(int)
     data = data[:,1:]
-    #print(names[:10])
-    print(data[:1])
-    print(target[:10])
+    #debug(names[:10])
+    debug(data[:1])
+    debug(target[:10])
 
     weight = numpy.zeros_like(target, dtype=float)
     neg = float(sum(target==0))
     pos = float(sum(target==1))
-    print(neg, pos)
+    debug(neg, pos)
     weight[target==0] = (neg+pos)/2 / neg
     weight[target==1] = (neg+pos)/2 / pos
     # the weights should still sum up to len(data)
-    print(sum(weight), len(data))
+    debug(sum(weight), len(data))
 
     rf = sklearn.ensemble.RandomForestClassifier(n_estimators=10)
-    print(rf)
+    debug(rf)
     rf = rf.fit(data, target, sample_weight=weight)
 
-    print(rf.score(data, target))
+    debug(rf.score(data, target))
 
     filename = "small.txt"
     names = numpy.loadtxt(filename, skiprows=1, usecols=[0], dtype=str)
@@ -37,9 +40,9 @@ def main():
     testtarget = testdata[:,0].astype(int)
     testdata = testdata[:,1:]
 
-    print(rf.score(testdata, testtarget))
-    print("neg acc:", rf.score(testdata[testtarget==0], testtarget[testtarget==0]))
-    print("pos acc:", rf.score(testdata[testtarget==1], testtarget[testtarget==1]))
+    debug(rf.score(testdata, testtarget))
+    debug("neg acc:", rf.score(testdata[testtarget==0], testtarget[testtarget==0]))
+    debug("pos acc:", rf.score(testdata[testtarget==1], testtarget[testtarget==1]))
 
     for name, prob in zip(names, rf.predict_proba(testdata)):
         print(name, prob[1], sep=",")
